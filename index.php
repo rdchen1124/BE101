acdeecdcc7e72c80d2b500b4eb3a7735ef42e6f3
rdchen1124 <rdchen1124@gmail.com>
2021-01-31 14:21:28 +0800

    Seperate index.php to conn.php and index.php to realized connect_module.

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php 
-//此為PHP連接 SQL 的範例
-$server = 'localhost';//檔案庫的Server網址
-$user = 'ryan';//account
-$password = 'ryan';//password
-$db_name = 'ryan';//database
-$conn = new mysqli($server, $user, $password, $db_name);//建立連線成功!!
+//使用conn.php(連線模組)來連接 SQL 的範例
+require_once("conn.php");
 print_r($conn);
 echo "<br>";
 //echo $conn->connect_error;
 if(empty($conn->connect_error)){
-    echo "資料庫連線成功!!"."<br>";
+    die("資料庫連線成功!!"."<br>");
 }
 ?>
