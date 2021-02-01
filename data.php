cac0dfaff7172ef640a1f624c884285dd944ec65
rdchen1124 <rdchen1124@gmail.com>
2021-02-01 22:23:40 +0800

    Setup for php to get data from mysql and then send new data by GET method.

---
+++ data.php
@@ -0,0 +1,6 @@
+<?php
+if(empty($_GET['name'])||empty($_GET['age'])){
+    echo "資料有誤, 麻煩請填寫正確再送出";
+    exit();
+}
+>
--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php 
 //使用conn.php(連線模組)來連接 SQL 的範例
 require_once("conn.php");
-print_r($conn);
-echo "<br>";
-//echo $conn->connect_error;
-if(empty($conn->connect_error)){
-    die("資料庫連線成功!!"."<br>");
+if(!empty($conn->connect_error)){
+    die("資料庫連線錯誤!!"."<br>");
+}
+else{
+    echo "資料庫連線成功!!"."<br>";
+}
+
+$result = $conn->query("SELECT * FROM `users`;");
+while ($row = $result->fetch_assoc()) {
+    echo "id : ".$row['id']."<br>";
+    echo "username : ".$row['username']."<br>";
 }
 ?>
+
+<form method='GET' actoin="data.php">
+    name : <input name='name' />
+    age : <input name='age' />
+    <input type='submit' />
+</form>
